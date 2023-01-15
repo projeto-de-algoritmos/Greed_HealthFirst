3b5cd73e88daa8e789720f76533da78474e4e106
Ailton Aires <ailtonaires5@gmail.com>
2023-01-15 13:42:38 -0300

    feat: add front

--- src/components/Mochila.jsx
+++ src/components/Mochila.jsx
@@ -61,7 +61,114 @@ function Mochilas() {
 
   return(
   <div className="main">
-  
+    <div className="container">
+      <p>Itens</p>
+      {items.map((item, index) => {
+        return (
+          <div key={item.id}>
+            <form class="">
+              <div class="q_div">
+                <label for="R$2">
+                  {item.name}
+                  <input
+                    min={0}
+                    class="q_inp"
+                    id={`qty_${item.id}`}
+                    placeholder="Quantidade"
+                    type="number"
+                    onChange={(e) => {
+                      const newItems = [...items];
+                      newItems[item.id].qty = Number(e.target.value);
+                      setItems(newItems);
+                      setResult(null);
+                    }}
+                    defaultValue={q_item[index] ? q_item[index] : item.qty}
+                  />
+                </label>
+                <label for="R$2">
+                  Peso (g)
+                  <input
+                    min={1}
+                    class="q_inp"
+                    id={`peso_${item.id}`}
+                    placeholder="R$2"
+                    type="number"
+                    defaultValue={item.peso}
+                    onChange={(e) => {
+                      const newItems = [...items];
+                      newItems[item.id].peso = Number(e.target.value);
+                      setItems(newItems);
+                      setResult(null);
+                    }}
+                  />
+                </label>
+
+                <label for="R$2">
+                  Priority
+                  <input
+                    class="q_inp"
+                    id={`priority_${item.id}`}
+                    placeholder="R$2"
+                    type="number"
+                    defaultValue={item.priority}
+                    onChange={(e) => {
+                      const newItems = [...items];
+                      newItems[item.id].priority = Number(e.target.value);
+                      setItems(newItems);
+                      setResult(null);
+                    }}
+                  />
+                </label>
+              </div>
+            </form>
+          </div>
+        );
+      })}
+      <form class="inp-container">
+        <input
+          placeholder="Seu peso em KG"
+          type="number"
+          id="peso_user"
+          class="inp"
+          min={40}
+        />
+      </form>
+
+      <input type="button" class="btn" value="Calcular" onClick={findPeso} />
+      <div class="result">
+        <p id="result"></p>
+      </div>
+    </div>
+    {result && (
+      <div className="container">
+        <p>Resultado</p>
+        <div className="result">
+          <div>
+            <p style={{ fontSize: "20px" }}>Item - Ícone - Peso - Quantidade</p>
+          </div>
+          {result.map((item, index) => {
+            return (
+              <div key={index}>
+                <p style={{ fontSize: "20px" }}>
+                  {item.name} - {item.icon} - {item.peso}g - {item.used_qty}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <p style={{ fontSize: "20px" }}>
+            Peso total:{" "}
+            {result.reduce((acc, item) => {
+              return acc + item.peso * item.used_qty;
+            }, 0)}
+            g
+          </p>
+
+          <p style={{ fontSize: "20px" }}>Peso restante: {peso_recomendado}g</p>
+        </div>
+      </div>
+    )}
   </div>);
 }
 
